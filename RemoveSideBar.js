51376c7dde2a8c72f8125010ffcd8ffbbfd73ceb
subodhbahl <Subodh.bahl@gmail.com>
2013-04-22 12:11:49 -0400

    Inital commit

---
+++ RemoveSideBar.js
@@ -0,0 +1,1 @@
+<span id="removeQASSidebar"></span><script type="text/javascript">    $(document).ready(function() {	 $('#sidebarDiv #removeQASSidebar').parent().parent().hide(); $('#sidebarDiv div:contains("QAS")').hide(); $('#sidebarDiv div:contains("RecordLabelChange")').hide(); });</script>
